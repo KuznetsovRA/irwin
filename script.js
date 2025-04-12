0f5bfdbe01c4957a398e3e9b0a78a302a73b4430
Kusnetsov Roman <KusnetsovRA@gmail.com>
2025-04-13 02:54:25 +0300

    2 commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const btns = document.querySelectorAll('.click-btn');
+
+btns.forEach(btn => {
+	btn.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		window.location.href = 'https://1wqjnb.com/v3/aggressive-casino?p=wpyi';
+	});
+});
